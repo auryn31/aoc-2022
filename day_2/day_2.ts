c6b3c819ae93301db9b1448e0f0fa69744681af7
Auryn Engel <auryn_engel@mckinsey.com>
2022-12-03 11:46:50 +0100

    add day 2 b

--- day_2/day_2.ts
+++ day_2/day_2.ts
@@ -11,12 +11,6 @@
 
 const valueFromSymbol = (symbol: string): number => {
   switch (symbol) {
-    case 'A':
-      return 1;
-    case 'B':
-      return 2;
-    case 'C':
-      return 3;
     case 'X':
       return 1;
     case 'Y':
@@ -53,11 +47,42 @@ const valueFromGame = (round: string): number => {
   }
 };
 
+// x -> loose
+// y -> draw
+// z -> win
+const valueFromGamePartB = (round: string): number => {
+  switch (round) {
+    case 'A X':
+      return 0 + 3;
+    case 'A Y':
+      return 3 + 1;
+    case 'A Z':
+      return 6 + 2;
+    case 'B X':
+      return 0 + 1;
+    case 'B Y':
+      return 3 + 2;
+    case 'B Z':
+      return 6 + 3;
+    case 'C X':
+      return 0 + 2;
+    case 'C Y':
+      return 3 + 3;
+    case 'C Z':
+      return 6 + 1;
+    default:
+      return 0;
+  }
+};
+
 console.log('Day 2 ✨');
 const input = await Deno.readTextFile('./day_2/day_2.txt');
 const games = input.split('\n');
 
-const result = games
+const result_a = games
   .map((it) => valueFromGame(it) + valueFromSymbol(it.split(' ')[1]))
   .reduce((a, b) => a + b, 0);
-console.log(`Result Part A: ${result}`);
+console.log(`Result Part A: ${result_a}`);
+const result_b = games.map(valueFromGamePartB).reduce((a, b) => a + b, 0);
+
+console.log(`Result Part B: ${result_b}`);
