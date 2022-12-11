0a8704e25f04e86d8944c6ab097b1919d347dd25
Auryn Engel <auryn_engel@mckinsey.com>
2022-12-11 18:07:27 +0100

    part b

--- day_11/index.ts
+++ day_11/index.ts
@@ -1,5 +1,6 @@
 const input_test = await Deno.readTextFile('./day_11/input.txt');
 
+const divisors: number[]= []
 type Monkey = {
   id: number;
   items: number[];
@@ -43,6 +44,7 @@ const parseOperation = (line: string): ((it: number) => number) => {
 const parseTest = (block: string): ((it: number) => number) => {
   const lines = block.split('\n')
   const divisor = Number(lines[3].split(' ')[5])
+  divisors.push(divisor)
   const nextTrue = Number(lines[4].split(' ')[9])
   const nextFalse = Number(lines[5].split(' ')[9])
   return (it: number) => (it%divisor == 0 ? nextTrue : nextFalse)
@@ -61,8 +63,33 @@ const makeMove = (monkeys: Monkey[]) => {
     }
   }
 }
+
+const MOD_CONSTAND = divisors.reduce((a, b) => a*b, 1) 
+const makeMoveB = (monkeys: Monkey[]) => {
+  for(const monkey of monkeys) {
+    while(monkey.items.length > 0) {
+      const item = monkey.items.shift() as number
+      const newValue = monkey.operation(item)
+      const nextValue = newValue%MOD_CONSTAND
+      const nextMonkey = monkey.test(newValue)
+      monkeys[nextMonkey].items.push(nextValue)
+      monkey.activity++
+    }
+  }
+}
+
+// Part A
+/*
 for(let i =0; i< 20; i++) {
   makeMove(monkeys)
+}
+console.log(monkeys)
+console.log(monkeys.sort((a, b) => b.activity - a.activity).slice(0, 2).reduce((a, b) => a * b.activity, 1))
+*/
+
+// Part B
+for(let i = 0; i< 10000; i++) {
+  makeMoveB(monkeys)
 }
 console.log(monkeys)
 console.log(monkeys.sort((a, b) => b.activity - a.activity).slice(0, 2).reduce((a, b) => a * b.activity, 1))
