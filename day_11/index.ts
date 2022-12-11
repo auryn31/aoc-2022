06105eab9b64496fa40914e7bf673d8792e9661b
Auryn Engel <auryn_engel@mckinsey.com>
2022-12-11 17:25:40 +0100

    day 11 part a

---
+++ day_11/index.ts
@@ -0,0 +1,68 @@
+const input_test = await Deno.readTextFile('./day_11/input.txt');
+
+type Monkey = {
+  id: number;
+  items: number[];
+  operation: (it: number) => number;
+  test: (it: number) => number;
+  activity: number
+}
+
+const parseInputFile = (input: string): Monkey[] => {
+
+  const monkeys: Monkey[]= []
+  for(const line of input.split('\n\n')) {
+    const parts = line.split('\n')
+    const id = Number(parts[0].split(' ')[1].split('')[0])
+    const items = parts[1].split(':')[1].split(',').map(Number)
+    const operation = parseOperation(parts[2])
+    const test = parseTest(line)
+    monkeys.push({
+      id,
+      items,
+      operation,
+      test,
+      activity: 0
+    })
+  }
+  return monkeys
+}
+
+const parseOperation = (line: string): ((it: number) => number) => {
+  const operation = line.split(':')[1]
+  const operationToApply = operation.split('=')[1]
+  const operant = operationToApply.split(' ')[2]
+  const num = Number(operationToApply.split(' ')[3])
+  switch(operant) {
+    case '*': return isNaN(num) ? (it: number) => it*it : (it: number) => it* num
+    case '+': return isNaN(num) ? (it: number) => it*it : (it: number) => it+ num
+  }
+  throw "Could not parse"
+}
+
+const parseTest = (block: string): ((it: number) => number) => {
+  const lines = block.split('\n')
+  const divisor = Number(lines[3].split(' ')[5])
+  const nextTrue = Number(lines[4].split(' ')[9])
+  const nextFalse = Number(lines[5].split(' ')[9])
+  return (it: number) => (it%divisor == 0 ? nextTrue : nextFalse)
+}
+
+const monkeys = parseInputFile(input_test)
+
+const makeMove = (monkeys: Monkey[]) => {
+  for(const monkey of monkeys) {
+    while(monkey.items.length > 0) {
+      const item = monkey.items.shift() as number
+      const newItemValue = Math.floor(monkey.operation(item)/3)
+      const nextMonkey = monkey.test(newItemValue)
+      monkeys[nextMonkey].items.push(newItemValue)
+      monkey.activity++
+    }
+  }
+}
+for(let i =0; i< 20; i++) {
+  makeMove(monkeys)
+}
+console.log(monkeys)
+console.log(monkeys.sort((a, b) => b.activity - a.activity).slice(0, 2).reduce((a, b) => a * b.activity, 1))
